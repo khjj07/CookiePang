680a83859f805bf4a1dd910b42ec0cc2e474921c
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-04-03 15:12:52 +0900

    속도조절 버튼 텍스트 수정

--- Assets/CookiePang/Scripts/UI/TimeScaleUpButton.cs
+++ Assets/CookiePang/Scripts/UI/TimeScaleUpButton.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 public class TimeScaleUpButton : Button
 {
+    private void Awake()
+    {
+        GetComponentInChildren<Text>().text = "Speed x1";
+    }
     public override void OnPointerClick(PointerEventData pointerEventData)
     {
         base.OnPointerClick(pointerEventData);
         if (!GameManager.instance.isTimeScaleUp)
         {
             GameManager.instance.isTimeScaleUp = true;
+            GetComponentInChildren<Text>().text = "Speed x3";
         }
         else
         {
             GameManager.instance.isTimeScaleUp = false;
+            GetComponentInChildren<Text>().text = "Speed x1";
         }
         
     }
