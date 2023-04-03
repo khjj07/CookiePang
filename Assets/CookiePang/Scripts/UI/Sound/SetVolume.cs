6bd721bb73d02bc31f27532cf7c46cd458a9cd5b
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-04-03 19:41:49 +0900

    사운드 문제해결

--- Assets/CookiePang/Scripts/UI/Sound/SetVolume.cs
+++ Assets/CookiePang/Scripts/UI/Sound/SetVolume.cs
@@ -9,11 +9,13 @@ public class SetVolume : MonoBehaviour
     public void BgmSlider()
     {
         SoundManager.instance.SetVolume(0, slider.value);
-        SoundManager.instance.GetComponent<AudioSource>().volume = slider.value;
+        //SoundManager.instance.GetComponent<AudioSource>().volume = slider.value;
+        SoundManager.instance.GetComponentsInChildren<AudioSource>()[0].volume = slider.value;
     }
     public void SfxSlider()
     {
         SoundManager.instance.SetVolume(1, slider.value);
+        SoundManager.instance.GetComponentsInChildren<AudioSource>()[1].volume = slider.value;
     }
     private void Update()
     {
