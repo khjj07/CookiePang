a3a1c53894b2f45efce70380730b8a77ff668b59
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-04-27 18:41:51 +0900

    ui및 여러 작업추가

--- Assets/CookiePang/Scripts/UI/TimeScaleUpButton.cs
+++ Assets/CookiePang/Scripts/UI/TimeScaleUpButton.cs
@@ -5,16 +5,19 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 public class TimeScaleUpButton : Button
 {
+    public Sprite[] sprites;
     public override void OnPointerClick(PointerEventData pointerEventData)
     {
         base.OnPointerClick(pointerEventData);
         if (!GameManager.instance.isTimeScaleUp)
         {
             GameManager.instance.isTimeScaleUp = true;
+            this.GetComponent<Image>().sprite = sprites[0];
         }
         else
         {
             GameManager.instance.isTimeScaleUp = false;
+            this.GetComponent<Image>().sprite = sprites[1];
         }
         
     }
