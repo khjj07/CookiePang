3815ceb81034003f5cc871b9739f5e0e6eaf6a6b
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-04-29 00:44:28 +0900

    마카롱 사운드 추가

--- Assets/CookiePang/Scripts/Ingame/Block/MacaroonBlock.cs
+++ Assets/CookiePang/Scripts/Ingame/Block/MacaroonBlock.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class MacaroonBlock : Block
 {
+    public virtual void Hit(int damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            GetComponent<Collider2D>().enabled = false;
+            SoundManager.instance.PlaySound(1, "MacaroonBlockSound");
+            GameManager.instance.DeleteBlock(this);
+        }
+    }
     public override BlockData ToData(int row, int column)
     {
         MacaroonBlockData data = new MacaroonBlockData(hp,row, column);
