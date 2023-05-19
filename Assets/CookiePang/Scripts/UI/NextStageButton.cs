5711ec117d68fea81e2fe58c70cd433795839083
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-05-19 17:16:32 +0900

    광고 id 수정(실제광고 id임)

--- Assets/CookiePang/Scripts/UI/NextStageButton.cs
+++ Assets/CookiePang/Scripts/UI/NextStageButton.cs
@@ -19,7 +19,7 @@ public class NextStageButton : MonoBehaviour
     }
     // These ad units are configured to always serve test ads.
 #if UNITY_ANDROID
-    private string _adUnitId = "ca-app-pub-3940256099942544/1033173712";
+    private string _adUnitId = "ca-app-pub-9046067694237697/3809009530";
 #elif UNITY_IPHONE
   private string _adUnitId = "ca-app-pub-3940256099942544/4411468910";
 #else
